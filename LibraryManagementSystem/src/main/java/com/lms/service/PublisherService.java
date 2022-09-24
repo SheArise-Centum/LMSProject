4f0cdbfc7236a784dfb9bfecba088930aec1ce44
SheArise <RAJESH@DESKTOP-OB3MQFV>
2022-09-24 16:10:23 +0530

    first commit

---
+++ LibraryManagementSystem/src/main/java/com/lms/service/PublisherService.java
@@ -0,0 +1,19 @@
+package com.lms.service;
+
+import java.util.List;
+
+import com.lms.entity.Publisher;
+
+public interface PublisherService {
+
+	public List<Publisher> findAllPublishers();
+
+	public Publisher findPublisherById(Long id);
+
+	public void createPublisher(Publisher publisher);
+
+	public void updatePublisher(Publisher publisher);
+
+	public void deletePublisher(Long id);
+
+}
